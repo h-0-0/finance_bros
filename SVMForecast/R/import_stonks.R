2e69cd9d8e99793e8a792d193d7c8861052c7ccd
DylanDijk <dylan_2103@hotmail.com>
2023-01-12 12:30:21 +0000

    working import_stonks func

--- SVMForecast/R/import_stonks.R
+++ SVMForecast/R/import_stonks.R
@@ -1,29 +1,43 @@
+
 #' import_stonks
 #'
-#' @param stocks
+#' @param stock_outcome Character. Outcome variable we will try to predict. Default: "BTC-USD"
+#' @param stock_pred Character Vector. Stock prices we will use as predictors
+#' @param day_lag Numeric Vector. The day lags of the predictors we want in dataset.
+#' @param from Date. The start date we want data from the outcome variable.
 #'
 #' @return
+#'   [`data.frame`] Containing outcome variable adjusted stock price and other Stocks selected as predictors with time lag selected by `day_lag` argument.
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#'   head(import_stonks(day_lag = c(1,2,3)))
+#' }
 import_stonks = function(stock_outcome = c("BTC-USD"), stock_pred =  c("ETH-USD", "DOGE-USD"), day_lag = c(1), from = as.Date("2018-01-02")){
 
   prices_out = tidyquant::tq_get(stock_outcome, from = from)
   prices_out = prices_out[,c("symbol", "date", "adjusted")]
+  prices_out =
+    prices_out %>%
+    pivot_wider(names_from = symbol, values_from = adjusted)
 
-
+  # i = stock_pred
   stagger_stock = function(i){
     prices_pred = tidyquant::tq_get(i, from = from - max(day_lag))
     prices_pred = prices_pred[,c("symbol", "date", "adjusted")]
-
+    colnames(prices_pred)[3] = paste(prices_pred$symbol[1])
+    prices_pred = prices_pred[,3]
     # day_lag = c(1,2,3)
     stagger_stock = vector(length = max(day_lag), "list")
 
     for(j in day_lag){
       if((max(day_lag) -j) == 0){
-        stagger_stock[[j]] = prices_pred[-c((nrow(prices_pred) - j):nrow(prices_pred)),]
+        stagger_stock[[j]] = prices_pred[-c((nrow(prices_pred) - j + 1):nrow(prices_pred)),]
+        colnames(stagger_stock[[j]]) = paste0(colnames(stagger_stock[[j]]),"-",j)
       } else {
-        stagger_stock[[j]] = prices_pred[-c(1:(max(day_lag) -j),(nrow(prices_pred) - j):nrow(prices_pred)),]
+        stagger_stock[[j]] = prices_pred[-c(1:(max(day_lag) -j),(nrow(prices_pred) - j +1):nrow(prices_pred)),]
+        colnames(stagger_stock[[j]]) = paste0(colnames(stagger_stock[[j]]),"-",j)
       }
 
     }
@@ -32,18 +46,14 @@ import_stonks = function(stock_outcome = c("BTC-USD"), stock_pred =  c("ETH-USD"
 
   }
 
-  lapply(stock_pred,  stagger_stock)
 
 
-  prices_adj = prices[,c("symbol", "date", "adjusted")]
-  prices_adj_wide =
-    prices_adj %>%
-    tidyr::pivot_wider(names_from = symbol, values_from = adjusted)
+  output = cbind(prices_out, dplyr::bind_cols(lapply(stock_pred,  stagger_stock)))
+  rownames(output) = output$date
+  output[,-1]
 
-  prices_adj_wide
-
+  return(output)
 
 }
 
 
-import_stonks()
