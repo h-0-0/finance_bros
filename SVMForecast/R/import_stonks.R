7e451ba5804556cbfb2cc19c3594aa30dc98f3a4
DylanDijk <dylan_2103@hotmail.com>
2023-01-13 19:46:54 +0000

    fixing import stonks

--- SVMForecast/R/import_stonks.R
+++ SVMForecast/R/import_stonks.R
@@ -19,7 +19,15 @@
 #' }
 import_stonks = function(stock_outcome = c("BTC-USD"), stock_pred =  c("ETH-USD", "DOGE-USD"), day_lag = c(1,2), from = as.Date("2018-01-02")){
 
-  prices_out = tq_get(stock_outcome, from = from)
+  # allign_fun =function(x){
+  #   vv = tq_get(x, from = from - max(day_lag), to = from - max(day_lag))
+  # }
+  # while (any(is.na(unlist(lapply(stock_pred, allign_fun))))) {
+  #   from = from + 1
+  #   print(from)
+  # }
+
+  prices_out = tq_get(stock_outcome, from = from - max(day_lag))
   prices_out = prices_out[,c("symbol", "date", "adjusted")]
   prices_out =
     prices_out %>%
@@ -33,28 +41,30 @@ import_stonks = function(stock_outcome = c("BTC-USD"), stock_pred =  c("ETH-USD"
     prices_pred = tq_get(i, from = from - max(day_lag))
     prices_pred = prices_pred[,c("symbol", "date", "adjusted")]
     colnames(prices_pred)[3] = paste(gsub(x = prices_pred$symbol[1], pattern = "-", replacement = "_"))
-    prices_pred = prices_pred[,3]
+    prices_pred = merge(prices_out, prices_pred, by="date", all.x = TRUE,)
+    prices_pred = prices_pred[4]
+    prices_pred = zoo::na.locf(prices_pred, na.rm = F)
     # day_lag = c(1,2,3)
     stagger_stock = vector(length = max(day_lag), "list")
 
     for(j in day_lag){
       if((max(day_lag) -j) == 0){
-        stagger_stock[[j]] = prices_pred[-c((nrow(prices_pred) - j + 1):nrow(prices_pred)),]
+        stagger_stock[[j]] = prices_pred[-c((nrow(prices_pred) - j + 1):nrow(prices_pred)),, drop = F]
         colnames(stagger_stock[[j]]) = paste0(colnames(stagger_stock[[j]]),"_",j)
       } else {
-        stagger_stock[[j]] = prices_pred[-c(1:(max(day_lag) -j),(nrow(prices_pred) - j +1):nrow(prices_pred)),]
+        stagger_stock[[j]] = prices_pred[-c(1:(max(day_lag) -j),(nrow(prices_pred) - j +1):nrow(prices_pred)),, drop = F]
         colnames(stagger_stock[[j]]) = paste0(colnames(stagger_stock[[j]]),"_",j)
       }
 
     }
 
-    bind_cols(stagger_stock)
+    dplyr::bind_cols(stagger_stock)
 
   }
 
 
 
-  output = cbind(prices_out, bind_cols(lapply(stock_pred,  stagger_stock)))
+  output = cbind(prices_out[-(1:max(day_lag)),], dplyr::bind_cols(lapply(stock_pred,  stagger_stock)))
   rownames(output) = output$date
 
   return(output[,-1])
